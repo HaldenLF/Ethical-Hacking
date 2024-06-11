8c26eb0e65a88277ef9f87d2e1b7aaa1e9440ac8
HaldenLF <Liamhalliden20@gmail.com>
2024-06-11 12:38:35 +0700

    Added comments to main

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import string
 
+# This function takes input from a user and checks to see if it 
+# can be used as a password and how strong it is if so.
 def password_Strength_Checker():
     password = input("Enter your password: \n"
                     "> ")
 
+    # checks for upper, lower, special and digit characters
     upper_case = any([1 if c in string.ascii_uppercase else 0 for c in password])
     lower_case = any([1 if c in string.ascii_lowercase else 0 for c in password])
     special = any([1 if c in string.punctuation else 0 for c in password])
@@ -14,6 +17,7 @@ def password_Strength_Checker():
 
     score = 0
 
+    # Checks input against commmon password list
     with open('pswd.txt', 'r') as f:
         common = f.read().splitlines()
 
@@ -22,6 +26,7 @@ def password_Strength_Checker():
               "-----------------------------------------------\n")
         check_another_password()
 
+    # Awards points for password length
     if length >= 8:
         score += 1
     if length >= 10:
@@ -33,6 +38,7 @@ def password_Strength_Checker():
 
     print(f"Password length is {str(length)}, adding {str(score)} points!\n")
 
+    # Displays password strength based upon score
     if sum(char) > 1:
         score += 1
     if sum(char) > 2:
@@ -59,6 +65,7 @@ def password_Strength_Checker():
                "-----------------------------------------------\n")
         check_another_password()
 
+# This function checks if the user wished to check another password or not
 def check_another_password():
     checkAnother = input("Do you want to a different password?"
                          "> ")
@@ -67,14 +74,15 @@ def check_another_password():
     elif checkAnother.lower() == "n" or checkAnother.lower() == "no":
         quit()
 
+# Main function to start the program
 def main():
     print("Welcome to the password checker!\n")
     userInput = input("Do you want to check if your password is valid?\n"
                       "> ")
-
     if userInput.lower() == "y" or userInput.lower() == "yes":
         password_Strength_Checker()
     elif userInput.lower() == "n" or userInput.lower() == "no":
         quit()
     
-main()
+if __name__ == "__main__":
+  main()

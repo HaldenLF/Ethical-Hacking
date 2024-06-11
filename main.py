108791a01660df8fec6d901b2d311edc1110fd1b
HaldenLF <Liamhalliden20@gmail.com>
2024-06-11 11:59:24 +0700

    Added recursive functions so that you can check multiple passwords without restarting the program each time

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import string
 
 def password_Strength_Checker():
-    password = input("What password do you want to check: \n"
+    password = input("Enter your password: \n"
                     "> ")
 
     upper_case = any([1 if c in string.ascii_uppercase else 0 for c in password])
@@ -18,8 +18,9 @@ def password_Strength_Checker():
         common = f.read().splitlines()
 
     if password in common:
-        print("Password found in common list. Score: 0/7")
-        exit()
+        print("Password found in common list. Score: 0/7\n"
+              "-----------------------------------------------\n")
+        check_another_password()
 
     if length >= 8:
         score += 1
@@ -30,7 +31,7 @@ def password_Strength_Checker():
     if length >= 14:
         score += 1
 
-    print(f"Password length is {str(length)}, adding {str(score)} points!")
+    print(f"Password length is {str(length)}, adding {str(score)} points!\n")
 
     if sum(char) > 1:
         score += 1
@@ -39,25 +40,41 @@ def password_Strength_Checker():
     if sum(char) > 3:
         score += 1
 
-    print(f"Password has {str(sum(char))} different characters, adding {str(sum(char) -1 )} points!")
+    print(f"Password has {str(sum(char))} different characters, adding {str(sum(char) -1 )} points!\n")
 
     if score < 4:
-        print(f"This password is weak! Score: {str(score)} / 7")
+        print(f"This password is weak! Score: {str(score)} / 7\n"
+              "-----------------------------------------------\n")
+        check_another_password()
     elif score == 4:
-        print(f"This password is below average strength! Score: {str(score)} / 7")
+        print(f"This password is below average strength! Score: {str(score)} / 7\n"
+              "-----------------------------------------------\n")
+        check_another_password()
     elif 4 < score < 6:
-        print (f"This password is average strength! Score: {str(score)} / 7")
+        print (f"This password is average strength! Score: {str(score)} / 7\n"
+               "-----------------------------------------------\n")
+        check_another_password()
     elif score > 6:
-        print (f"The password is strong! Score: {str(score)} / 7")
+        print (f"The password is strong! Score: {str(score)} / 7\n"
+               "-----------------------------------------------\n")
+        check_another_password()
 
 def check_another_password():
-    pass
+    checkAnother = input("Do you want to a different password?"
+                         "> ")
+    if checkAnother.lower() == "y" or checkAnother.lower() == "yes":
+        password_Strength_Checker()
+    elif checkAnother.lower() == "n" or checkAnother.lower() == "no":
+        quit()
 
 def main():
-    print("Welcome to the password checker!")
-    userInput = input("Do you want to check if your password is valid?")
+    print("Welcome to the password checker!\n")
+    userInput = input("Do you want to check if your password is valid?\n"
+                      "> ")
 
-    if userInput.lower() == "y" or choice.lower() == "yes":
+    if userInput.lower() == "y" or userInput.lower() == "yes":
         password_Strength_Checker()
-
-        
+    elif userInput.lower() == "n" or userInput.lower() == "no":
+        quit()
+    
+main()
